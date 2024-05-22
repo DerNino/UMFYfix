038dc2fcb2d53a1908ad8bc00fdae7dd6b6b65a5
SackUeli <162135768+SackUeli@users.noreply.github.com>
2024-05-22 23:09:49 +0200

    UMFY.py

--- Your_Project/UMFY.py
+++ Your_Project/UMFY.py
@@ -29,7 +29,7 @@ h1, h2, h3, h4, h5, h6 {
 st.markdown(page_bg, unsafe_allow_html=True)
 
 # Pfad zum Bild im selben Verzeichnis wie das Skript
-IMAGE_PATH = os.path.join(os.path.dirname(__file__), "logo.png")
+IMAGE_PATH = os.path.join(os.path.dirname(__file__), "UMFY Logo.png")
 
 # Funktion, um ein Bild in Base64 umzuwandeln
 def img_to_bytes(img):

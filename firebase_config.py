48bf8f22f656be086e77f7a224b8359180b0bd35
DerNino <126977851+DerNino@users.noreply.github.com>
2024-05-22 18:58:44 +0200

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Pfad zu deinem Service Account Key JSON
-cred = credentials.Certificate(r"https://github.com/DerNino/UMFYfix/blob/main/umfy-29ddb-firebase-adminsdk-gbags-5dbe02c8d5.json")
-
-# Initialisiere die App mit den Service Account Credentials
+# Path to your local service account key file using raw string
+cred = credentials.Certificate(r"C:\Users\perre\Documents\GitHub\UMFYfix\umfy-29ddb-firebase-adminsdk-gbags-2dda4d0a53.json")
 firebase_admin.initialize_app(cred)
 
-# Firestore-Datenbank initialisieren
 db = firestore.client()

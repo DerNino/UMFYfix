b470acb20f60b62b9f010a94a66022415bb60708
DerNino <126977851+DerNino@users.noreply.github.com>
2024-05-22 19:48:54 +0200

    Update firebase_config.py

--- Your_Project/firebase_config.py
+++ Your_Project/firebase_config.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore
+import json
 
+# Load Firebase credentials from Streamlit secrets
+firebase_config = st.secrets["firebase"]
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()

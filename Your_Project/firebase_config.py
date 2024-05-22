f6a2f1940b1ebe5d726c3e47bdadc65b22653166
SackUeli <162135768+SackUeli@users.noreply.github.com>
2024-05-22 22:36:20 +0200

    firebase_config.py

--- Your_Project/firebase_config.py
+++ Your_Project/firebase_config.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    try:
+    try
 # Load Firebase credentials from Streamlit secrets
 firebase_config = {
     "type": st.secrets["firebase"]["type"],
